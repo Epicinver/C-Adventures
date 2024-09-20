b01a9170b0f1747dfe945be7a68f71f716386a6f
Epicinver <arran23130@gmail.com>
2024-09-20 15:58:31 +0100

    Create script.js

---
+++ level3/script.js
@@ -0,0 +1,8 @@
+function runVariables() {
+    const age = 10;
+    const height = 1.75;
+    const initial = 'C';
+    
+    const output = `Age: ${age}\nHeight: ${height} meters\nInitial: ${initial}`;
+    document.getElementById('output2').textContent = output;
+}
