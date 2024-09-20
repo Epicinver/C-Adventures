f97b4de3739056a4b304f9e2bc0ec0914e45bbca
Epicinver <arran23130@gmail.com>
2024-09-20 16:02:53 +0100

    Delete level3 directory

--- level3/script.js
+++
@@ -1,8 +0,0 @@
-function runVariables() {
-    const age = 10;
-    const height = 1.75;
-    const initial = 'C';
-    
-    const output = `Age: ${age}\nHeight: ${height} meters\nInitial: ${initial}`;
-    document.getElementById('output2').textContent = output;
-}
